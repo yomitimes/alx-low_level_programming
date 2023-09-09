ee11e6b8cb32a153a920b01b21786134004c9bc6
Yomi Abolaji <yomitimes@gmail.com>
2023-09-09 03:27:01 -0700

    New Project

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - A program that prints the size of various types
+ * Return: 0 (Successful)
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+
+	sizeof(a);
+	sizeof(b);
+	sizeof(c);
+	sizeof(d);
+	sizeof(e);
+	return (0);
+}
