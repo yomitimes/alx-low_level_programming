bcdb363eb741be84d182404c9153b5f6dc3177c4
Yomi Abolaji <yomitimes@gmail.com>
2023-09-09 02:31:46 -0700

    New Project

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
-/* main - Entry point */
-/* return: 0 if successful.*/
+/** 
+ * main - A program that prints a line with puts function
+ *  Return: 0 (Success)
+ *
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-/* main - entry point; Print a phrase*/
+/* main - entry point; Print a phrase with printf main function*/
 /* return: 0(successful)*/
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,/n);
-	return 0;
+	return (0);
 }
