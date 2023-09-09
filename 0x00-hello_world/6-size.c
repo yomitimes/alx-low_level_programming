e453fad6ae7ac001262e4eac5ec8d84f4ae773af
Yomi Abolaji <yomitimes@gmail.com>
2023-09-09 04:12:39 -0700

    New Project

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,9 +12,9 @@ int main(void)
 	float e;
 
 	printf("Size of a char: %lu bytes(s)\n", (unsigned long)sizeof(a));
-	printf("Size of a char: %lu bytes(s)\n", (unsigned long)sizeof(b));
-	printf("Size of a char: %lu bytes(s)\n", (unsigned long)sizeof(c));
-	printf("Size of a char: %lu bytes(s)\n", (unsigned long)sizeof(d));
-	printf("Size of a char: %lu bytes(s)\n", (unsigned long)sizeof(e));
+	printf("Size of an int: %lu bytes(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a long int: %lu bytes(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a long long int: %lu bytes(s)\n", (unsigned long)sizeof(d));
+	printf("Size of a float: %lu bytes(s)\n", (unsigned long)sizeof(e));
 	return (0);
 }
