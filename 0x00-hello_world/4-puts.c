f662ccbbf1c16851d6efa58cf1c628980001ecdf
Yomi Abolaji <yomitimes@gmail.com>
2023-09-09 02:24:50 -0700

    New Project

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-/* main: A programn to print.*/
+/* main - Entry point */
 /* return: 0 if successful.*/
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
