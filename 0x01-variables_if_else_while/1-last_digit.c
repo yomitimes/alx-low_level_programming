91699eb24ce821341de2028098d026728e40aefa
Yomi Abolaji <yomitimes@gmail.com>
2023-09-10 15:53:39 -0700

    new

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - A program that tells whether a number is positive or negative.
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	int last;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	last = n % 10;
+	if (last == 0)
+	{
+	printf("Last digit of %d is %d and is 0", n, last);
+	}
+	else if (last < 6)
+	{
+	printf("Last digit of %d is %d and is less than 6 and not 0", n, last);
+	}
+	else if (last > 5)
+	{
+	printf("Last digit of %d is %d and is greater than 5", n, last);
+	}
+	return (0);
+}
