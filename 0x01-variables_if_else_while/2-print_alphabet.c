7a90cb09dff9a98e60234c23b814da4b4d007a2a
Yomi Abolaji <yomitimes@gmail.com>
2023-09-11 12:39:44 -0700

    New

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - A program that prints lowercase alphabet.
+ * Return: 0 (success)
+ */
+int main(void)
+{
+char i;
+
+for (i = 'a'; i <= 'z'; i++)
+putchar(i);
+putchar("/n");
+return (0);
+}
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - A program that prints lowercase alphabet.
+ * Return: 0 (success)
+ */
+int main(void)
+{
+char i, j;
+
+for (i = 'a'; i <= 'z'; i++)
+putchar(i);
+for (j = 'A'; j <= 'Z'; j++)
+putchar(j);
+putchar("/n");
+return (0);
+}
+putchar("/n");
+return (0);
+}
