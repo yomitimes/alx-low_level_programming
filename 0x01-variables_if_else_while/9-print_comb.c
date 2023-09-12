22afd48a5447f22cc1c527b3787b96570e815d5e
Yomi Abolaji <yomitimes@gmail.com>
2023-09-12 01:43:12 -0700

    New

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
 /**
- * main - A program that prints numbers.
- * Return: 0 (Success)
+ * main- prints all possible combinations of single-digit numbers
+ *
+ * Return: Always 0.
+ *
  */
 int main(void)
 {
-int i;
+int num;
 
-for (i = '0'; i <= '8'; i++)
+for (num = 0; num <= 9; num++)
 {
-putchar(i);
+putchar((num % 10) + '0');
+if (num == 9)
+continue;
 putchar(',');
 putchar(' ');
-if (i == '8')
-putchar('9');
 }
+putchar('\n');
 return (0);
 }
