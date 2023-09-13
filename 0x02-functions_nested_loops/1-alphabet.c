9d79f6bb4a76f440a3fe780614fabe02e18833f4
Yomi Abolaji <yomitimes@gmail.com>
2023-09-13 03:55:47 -0700

    New

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - A program that prints the alphabet
+ * Return: 0
+ */
+int main(void)
+{
+char i;
+for (i = 'a'; i <= 'z'; i++)
+_putchar(i);
+_putchar('\n');
+return (0);
+}
